2dece9ff88a748f21202b1dc2005ad8f17f1c148
iuliia_gorshkova <gorshkovayulia93@gmail.com>
2020-11-19 15:53:00 +0300

    Some changes were added.

--- plate.py
+++ plate.py
@@ -1,38 +1,47 @@
-class Dimensions():
+class Dimensions:
 
     def __init__(self, number_of_rows, number_of_columns):
         """Initiate attributes for a plate"""
         self.number_of_rows = number_of_rows
         self.number_of_columns = number_of_columns
 
-class PlateCell():
+    def get_capacity(self):
+        """Calculate the size of the plate"""
+        plate_size = self.number_of_rows * self.number_of_columns
+        return plate_size
+
+
+class PlateCell:
 
     def __init__(self, cell_number, dimensions):
-        self.cell_number = int(cell_number)
+        self.cell_number = cell_number
         self.dimensions = dimensions
+        if not self.cell_number:
+            raise TypeError("Cell number cannot be None!")
+        if type(self.cell_number) is not int:
+            raise TypeError("Cell number should be int!")
+        if self.cell_number < 0:
+            raise ValueError("Сell number cannot be negative!")
+        if self.cell_number == 0:
+            raise ValueError("Сell number cannot equal zero!")
+        if self.cell_number > self.dimensions.get_capacity():
+            raise ValueError(str(self.cell_number) + " cell number is too big for " + "(" + str(
+                self.dimensions.number_of_rows) + ", " + str(self.dimensions.number_of_columns) + ")" + " dimension!")
 
     def calculate_row_and_column(self):
-        """Calculate row and column based on cell number"""
-        if self.cell_number > self.dimensions.number_of_rows * self.dimensions.number_of_columns:
-            raise AttributeError("Cell number is too big for current dimension!")
-        elif self.cell_number < 0:
-            raise AttributeError("Cell number cannot be negative!")
-        else:
-            self.row = (self.cell_number % self.dimensions.number_of_rows)
-            self.column = (self.cell_number // self.dimensions.number_of_rows) % self.dimensions.number_of_columns
-            return [self.row, self.column]
+        """
+        Calculate row and column based on cell number.
+        The numbering goes from top to bottom (not left to right).
+        """
+        row = (self.cell_number - 1) % self.dimensions.number_of_rows
+        column = (self.cell_number - 1) // self.dimensions.number_of_rows
+        return row + 1, column + 1
 
     def as_string(self):
         """Return human readable coordinate"""
-        self.calculated_row_and_column = self.calculate_row_and_column()
+        (row, column) = self.calculate_row_and_column()
         letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
-        letter = letters[self.calculated_row_and_column[0]]
-        coordinate = letter + str(self.calculated_row_and_column[1] + 1)
-        return coordinate
-        # Old version -->
-        # for index, value in enumerate(letters):
-        #     if index == self.calculated_row_and_column[0]:
-        #         return str(value + str(self.calculated_row_and_column[1] + 1))
+        return letters[row - 1] + str(column)
 
     def to_higher_density(self):
         """
@@ -40,24 +49,17 @@ class PlateCell():
         :return: a new PlateCell, the one that corresponds to a higher density
                  (e.g. 96 if current was 24, 384 if current was 96, etc)
         """
-        self.calculated_row_and_column = self.calculate_row_and_column()
-        self.new_row = self.calculated_row_and_column[0] + 2
-        letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF']
-        letter = letters[self.new_row]
-        coordinate = letter + str(self.calculated_row_and_column[1] + 1)
-        return coordinate
-        # Old version -->
-        # for index, value in enumerate(letters):
-        #     if index == self.new_row:
-        #         return str(value + str(self.calculated_row_and_column[1] + 1))
+        (row, column) = self.calculate_row_and_column()
+        new_row = (row - 1) * 2
+        new_column = (column - 1) * 2
+        return(new_row + 1, new_column + 1)
 
-# dimensions = Dimensions(16, 24)
-# cell = PlateCell(385, dimensions)
+dimensions = Dimensions(16, 24)
+cell = PlateCell(2, dimensions)
 # test = cell.as_string()
+# print(cell.cell_number)
 # print(test)
-# test_2 = cell.to_higher_density()
+test_2 = cell.to_higher_density()
 # row_and_column = cell.calculate_row_and_column()
-# print(row_and_column)
+print(test_2)
 # cell.to_higher_density()        # returns 1536 plate cell (because 1536 is the next density), in this case: D01
-
-
--- tests_for_plate.py
+++ tests_for_plate.py
@@ -5,60 +5,69 @@ from plate import Dimensions
 class TestPlateCell(unittest.TestCase):
     """Tests for PlateCell class"""
 
-    """Tests for 'calculate_row_and_column' method"""
-
     def test_first_row_and_first_column_are_returned_for_the_first_cell(self):
         dimensions = Dimensions(16, 24)
-        cell = PlateCell(0, dimensions)
+        cell = PlateCell(1, dimensions)
         row_and_column = cell.calculate_row_and_column()
-        self.assertEqual([0, 0], row_and_column)
+        self.assertEqual((1, 1), row_and_column)
 
     def test_sixteenth_row_and_twelfth_column_are_returned_for_the_middle_cell(self):
         dimensions = Dimensions(16, 24)
-        cell = PlateCell(191, dimensions)
+        cell = PlateCell(192, dimensions)
         row_and_column = cell.calculate_row_and_column()
-        self.assertEqual([15, 11], row_and_column)
+        self.assertEqual((16, 12), row_and_column)
 
     def test_sixteenth_row_and_twenty_fourth_column_are_returned_for_the_last_cell(self):
         dimensions = Dimensions(16, 24)
-        cell = PlateCell(383, dimensions)
+        cell = PlateCell(384, dimensions)
         row_and_column = cell.calculate_row_and_column()
-        self.assertEqual([15, 23], row_and_column)
+        self.assertEqual((16, 24), row_and_column)
 
-    def test_zero_row_and_zero_column_are_returned_for_0_cell(self):
+    def test_zero_cell_number_raises_value_error(self):
         dimensions = Dimensions(16, 24)
-        cell = PlateCell(0, dimensions)
-        row_and_column = cell.calculate_row_and_column()
-        self.assertEqual([0, 0], row_and_column)
+        try:
+            cell = PlateCell(0, dimensions)
+            number = cell.cell_number()
+            self.fail("Should be an exception")
+        except ValueError as e:
+            str(e)
+            self.assertEqual("Сell number cannot equal zero!", str(e))
 
-    def test_correct_row_and_column_are_returned_for_decimal_cell_number(self):
-        dimensions = Dimensions(16, 24)
-        cell = PlateCell(1.65, dimensions)
-        row_and_column = cell.calculate_row_and_column()
-        self.assertEqual([1, 0], row_and_column)
-
-    def test_negative_cell_number_raises_error(self):
+    def test_negative_cell_number_raises_value_error(self):
         dimensions = Dimensions(16, 24)
         try:
             cell = PlateCell(-5, dimensions)
+            number = cell.cell_number()
             self.fail("Should be an exception")
-        except AttributeError as e:
+        except ValueError as e:
             str(e)
-        self.assertEqual("Cell number cannot be negative!", str(e))
+            self.assertEqual("Сell number cannot be negative!", str(e))
 
-    def test_not_existing_cell_number_raises_error(self):
+    def test_not_existing_cell_number_raises_value_error(self):
         dimensions = Dimensions(16, 24)
         try:
             cell = PlateCell(385, dimensions)
+            number = cell.cell_number()
             self.fail("Should be an exception")
-        except AttributeError as e:
+        except ValueError as e:
             str(e)
-            self.assertEqual("Cell number is too big for current dimension!", str(e))
+            self.assertEqual("385 cell number is too big for (16, 24) dimension!", str(e))
+
+    def test_decimal_cell_number_raises_value_error(self):
+        dimensions = Dimensions(16, 24)
+        try:
+            cell = PlateCell(1.65, dimensions)
+            number = cell.cell_number()
+            self.fail("Should be an exception")
+        except TypeError as e:
+            str(e)
+            self.assertEqual("Сell number should be int!", str(e))
 
     def test_type_error_appears_in_case_absent_cell_number(self):
         dimensions = Dimensions(16, 24)
         try:
             cell = PlateCell(dimensions)
+            number = cell.cell_number()
             self.fail("Should be an exception")
         except TypeError as e:
             str(e)
@@ -68,48 +77,42 @@ class TestPlateCell(unittest.TestCase):
         dimensions = Dimensions(16, 24)
         try:
             cell = PlateCell(None, dimensions)
+            number = cell.cell_number()
             self.fail("Should be an exception")
         except TypeError as e:
             str(e)
-            self.assertEqual("int() argument must be a string, a bytes-like object or a number, not 'NoneType'", str(e))
+            self.assertEqual("Cell number cannot be None!", str(e))
 
     def test_value_error_appears_in_case_cell_number_is_text(self):
         dimensions = Dimensions(16, 24)
         try:
             cell = PlateCell('test', dimensions)
+            number = cell.cell_number()
             self.fail("Should be an exception")
-        except ValueError as e:
+        except TypeError as e:
             str(e)
-            self.assertEqual("invalid literal for int() with base 10: 'test'", str(e))
+            self.assertEqual("Сell number should be int!", str(e))
 
-
-    """Tests for 'as_string' method"""
-
-    def test_correct_coordinate_for_the_first_cell_should_be_returned(self):
+    def test_A1_coordinate_is_returned_for_the_first_cell_on_384_plate(self):
         dimensions = Dimensions(16, 24)
-        cell = PlateCell(0, dimensions)
+        cell = PlateCell(1, dimensions)
         coordinate = cell.as_string()
         self.assertEqual('A1', coordinate)
 
-    def test_correct_coordinate_for_the_middle_cell_should_be_returned(self):
+    def test_P12_coordinate_is_returned_for_the_middle_cell_on_384_plate(self):
         dimensions = Dimensions(16, 24)
-        cell = PlateCell(191, dimensions)
+        cell = PlateCell(192, dimensions)
         coordinate = cell.as_string()
         self.assertEqual('P12', coordinate)
 
-    def test_correct_coordinate_for_the_last_cell_should_be_returned(self):
+    def test_P24_coordinate_is_returned_for_the_last_cell_on_384_plate(self):
         dimensions = Dimensions(16, 24)
-        cell = PlateCell(383, dimensions)
+        cell = PlateCell(384, dimensions)
         coordinate = cell.as_string()
         self.assertEqual('P24', coordinate)
 
-    def test_none_is_returned_in_case_row_is_equal_to_0(self):
-        dimensions = Dimensions(16, 24)
-        cell = PlateCell(0, dimensions)
-        coordinate = cell.as_string()
-        self.assertEqual(None, coordinate)
+    def test_A1_coordinate_is_returned_for_the_first_cell_on_1536_plate
 
-    # """Tests for 'to_higher_density' method"""
 
 if __name__ == "__main__":
     unittest.main()
